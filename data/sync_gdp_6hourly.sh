2f5c1a305d523c78c800f57ae3143f5dad767fff
Philippe Miron, Ph. D <philippemiron@gmail.com>
2022-01-25 14:14:20 -0500

    fix metadata and update preprocess scripts

---
+++ data/sync_gdp_6hourly.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REMOTE_GDP_PATH=/phod/pub/lumpkin/netcdf
+LOCAL_GDP_PATH=raw/6-hourly
+
+# declare folders list
+declare -a folders=("buoydata_1_5000" 
+		"buoydata_5001_10000"
+		"buoydata_10001_15000" 
+		"buoydata_15001_dec20"
+        )
+
+# synchronize each of the folders
+for folder in "${folders[@]}"
+do
+   lftp ftp.aoml.noaa.gov -e "mirror -e --ignore-time $REMOTE_GDP_PATH/$folder $LOCAL_GDP_PATH/$folder; quit"
+done
--- data/sync_gdp_hourly.sh
+++ data/sync_gdp_hourly.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-REMOTE_GDP_PATH=/phod/pub/lumpkin/hourly/v1.04/netcdf
-LOCAL_GDP_PATH=raw
+REMOTE_GDP_PATH=/phod/pub/lumpkin/hourly/v2.00/netcdf
+LOCAL_GDP_PATH=raw/v2.00
 
-
-# declare folders list
-declare -a folders=("argos_block1" 
-		    "argos_block2"
-		    "argos_block3" 
-		    "argos_block4"
-	    	"argos_block5"
-		    "argos_block6"
-		    "argos_block7"
-		    "argos_block8"
-			"gps")
-
-# synchronize each of the folders
-for folder in "${folders[@]}"
-do
-   lftp ftp.aoml.noaa.gov -e "mirror -e --ignore-time $REMOTE_GDP_PATH/$folder $LOCAL_GDP_PATH/$folder; quit"
-done
+lftp ftp.aoml.noaa.gov -e "mirror -e --ignore-time $REMOTE_GDP_PATH $LOCAL_GDP_PATH; quit"
