58ad33193cfc33d4c833de1d04b0f9253768c213
Philippe Miron, Ph. D <philippemiron@gmail.com>
2022-07-07 18:11:05 -0400

    fix linting

--- tests/select_tests.py
+++ tests/select_tests.py
@@ -8,6 +8,7 @@ from clouddrift import dataformat, select
 if __name__ == "__main__":
     unittest.main()
 
+
 def is_ak_empty(ds: ak.Array):
     if len(ds) == 0 and not ds.fields:
         return True
@@ -24,22 +25,26 @@ class select_tests(TestCase):
         rowsize = [5, 4, 2]
         longitude = [[-121, -111, 51, 61, 71], [12, 22, 32, 42], [103, 113]]
         latitude = [[-90, -45, 45, 90, 0], [10, 20, 30, 40], [10, 20]]
-        t = [[1,2,3,4,5], [2,3,4,5], [4,5]]
-        ids = [[1,1,1,1,1], [2,2,2,2], [3,3]]
-        test = [[True, True, True, False, False], [True, False, False, False], [False, False]]
+        t = [[1, 2, 3, 4, 5], [2, 3, 4, 5], [4, 5]]
+        ids = [[1, 1, 1, 1, 1], [2, 2, 2, 2], [3, 3]]
+        test = [
+            [True, True, True, False, False],
+            [True, False, False, False],
+            [False, False],
+        ]
         nb_obs = np.sum(rowsize)
         nb_traj = len(drifter_id)
         attrs_global = {
             "title": "test trajectories",
             "history": "version xyz",
         }
-        variables_coords = {"ids": "ids", "time": "t", "lon": "longitude", "lat": "latitude"}
-        coords = {
-            "longitude": longitude, 
-            "latitude": latitude,
-            "ids":  ids,
-            "t": t
+        variables_coords = {
+            "ids": "ids",
+            "time": "t",
+            "lon": "longitude",
+            "lat": "latitude",
         }
+        coords = {"longitude": longitude, "latitude": latitude, "ids": ids, "t": t}
         metadata = {"ID": drifter_id, "rowsize": rowsize}
         data = {"test": test}
 
@@ -72,10 +77,14 @@ class select_tests(TestCase):
             list_ds.append(
                 xr.Dataset(coords=xr_coords, data_vars=xr_data, attrs=attrs_global)
             )
-        
+
         # create test ragged array
         ra = dataformat.create_ragged_array(
-        [0, 1, 2], lambda i: list_ds[i], variables_coords, ["ID", "rowsize"], ["test"]
+            [0, 1, 2],
+            lambda i: list_ds[i],
+            variables_coords,
+            ["ID", "rowsize"],
+            ["test"],
         )
 
         self.ds = ra.to_awkward()
@@ -83,46 +92,48 @@ class select_tests(TestCase):
     def test_equal(self):
         ds_sub = select.subset(self.ds, {"test": True})
         self.assertEqual(len(ds_sub.ID), 2)
-    
+
     def test_select(self):
         ds_sub = select.subset(self.ds, {"ID": [1, 2]})
-        self.assertTrue(ak.all(ds_sub.ID == ak.Array([1,2])))
+        self.assertTrue(ak.all(ds_sub.ID == ak.Array([1, 2])))
         self.assertEqual(len(ds_sub.obs.lon), 2)
-        
+
     def test_range(self):
         # positive
         ds_sub = select.subset(self.ds, {"lon": (0, 180)})
-        self.assertTrue(ak.all(ds_sub.obs.lon[0] == ak.Array([51,61,71])))
+        self.assertTrue(ak.all(ds_sub.obs.lon[0] == ak.Array([51, 61, 71])))
         self.assertTrue(ak.all(ds_sub.obs.lon[1] == ak.Array([12, 22, 32, 42])))
         self.assertTrue(ak.all(ds_sub.obs.lon[2] == ak.Array([103, 113])))
-        
+
         # negative range
         ds_sub = select.subset(self.ds, {"lon": (-180, 0)})
         self.assertEqual(len(ds_sub.ID), 1)
         self.assertEqual(ds_sub.ID[0], 1)
         self.assertEqual(len(ds_sub.obs.lon), 1)
         self.assertTrue(ak.all(ds_sub.obs.lon[0] == ak.Array([-121, -111])))
-        
+
         # both
         ds_sub = select.subset(self.ds, {"lon": (-30, 30)})
         self.assertEqual(len(ds_sub.ID), 1)
         self.assertEqual(ds_sub.ID[0], 2)
         self.assertEqual(len(ds_sub.obs.lon), 1)
         self.assertTrue(ak.all(ds_sub.obs.lon[0] == ak.Array([12, 22])))
-    
+
     def test_combine(self):
-        ds_sub = select.subset(self.ds, {"ID": [1,2], "lat": (-90,20), "lon": (-180, 25), "test": True})
-        self.assertTrue(ak.all(ds_sub.ID == ak.Array([1,2])))
+        ds_sub = select.subset(
+            self.ds, {"ID": [1, 2], "lat": (-90, 20), "lon": (-180, 25), "test": True}
+        )
+        self.assertTrue(ak.all(ds_sub.ID == ak.Array([1, 2])))
         self.assertTrue(ak.all(ds_sub.obs.lon == ak.Array([[-121, -111], [12]])))
         self.assertTrue(ak.all(ds_sub.obs.lat == ak.Array([[-90, -45], [10]])))
-    
+
     def test_empty(self):
-        ds_sub = select.subset(self.ds, {"ID": 3, 'lon': (-180, 0)})
+        ds_sub = select.subset(self.ds, {"ID": 3, "lon": (-180, 0)})
         self.assertTrue(is_ak_empty(ds_sub))
-    
+
     def test_unknown_var(self):
         with self.assertRaises(ValueError):
             select.subset(self.ds, {"a": 10})
-            
+
         with self.assertRaises(ValueError):
-            select.subset(self.ds, {"lon": (0, 180), 'a': (0,10)})
+            select.subset(self.ds, {"lon": (0, 180), "a": (0, 10)})
