Fix the bearing formula to be defined as zero toward East and increasing counter-clockwise.
Co-authored-by: Jonathan Lilly <jonathanlilly@gmail.com>

clouddrift/analysis.py
@@ -71,9 +71,9 @@ def velocity_from_positions(
         elif coord_system == "spherical":
             distances = distance(y[:-1], x[:-1], y[1:], x[1:])
             bearings = bearing(y[:-1], x[:-1], y[1:], x[1:])
-            dx[:-1] = distances * np.sin(bearings)
+            dx[:-1] = distances * np.cos(bearings)
             dx[-1] = dx[-2]
-            dy[:-1] = distances * np.cos(bearings)
+            dy[:-1] = distances * np.sin(bearings)
             dy[-1] = dy[-2]
         else:
             raise ValueError('coord_system must be "spherical" or "cartesian".')
@@ -93,9 +93,9 @@ def velocity_from_positions(
         elif coord_system == "spherical":
             distances = distance(y[:-1], x[:-1], y[1:], x[1:])
             bearings = bearing(y[:-1], x[:-1], y[1:], x[1:])
-            dx[1:] = distances * np.sin(bearings)
+            dx[1:] = distances * np.cos(bearings)
             dx[0] = dx[1]
-            dy[1:] = distances * np.cos(bearings)
+            dy[1:] = distances * np.sin(bearings)
             dy[0] = dy[1]
         else:
             raise ValueError('coord_system must be "spherical" or "cartesian".')
@@ -118,10 +118,10 @@ def velocity_from_positions(
         elif coord_system == "spherical":
             distances = distance(y[:-2], x[:-2], y[2:], x[2:])
             bearings = bearing(y[:-2], x[:-2], y[2:], x[2:])
-            dx[1:-1] = distances * np.sin(bearings) / 2
+            dx[1:-1] = distances * np.cos(bearings) / 2
             dx[0] = dx[1]  # FIXME
             dx[-1] = dx[-2]  # FIXME
-            dy[1:-1] = distances * np.cos(bearings) / 2
+            dy[1:-1] = distances * np.sin(bearings) / 2
             dy[0] = dy[1]  # FIXME
             dy[-1] = dy[-2]  # FIXME
         else:

clouddrift/haversine.py
@@ -45,15 +45,15 @@ def distance(
 def bearing(
     lat1: np.ndarray, lon1: np.ndarray, lat2: np.ndarray, lon2: np.ndarray
 ) -> np.ndarray:
-    """Return elementwise Haversine initial (forward) bearing in radians from
-    arrays of latitude and longitude in degrees.
+    """Return elementwise initial (forward) bearing in radians from arrays of
+    latitude and longitude in degrees, based on the spherical law of cosines.
 
     The formula is:
 
-    θ = atan2(sin Δλ ⋅ cos φ2, cos φ1 ⋅ sin φ2 - sin φ1 ⋅ cos φ2 ⋅ cos Δλ)
+    θ = atan2(cos φ1 ⋅ sin φ2 - sin φ1 ⋅ cos φ2 ⋅ cos Δλ, sin Δλ ⋅ cos φ2)
 
     where (φ, λ) is (lat, lon) and θ is bearing, all in radians.
-    Bearing is defined as zero toward North and increasing clockwise.
+    Bearing is defined as zero toward East and increasing counter-clockwise.
 
     Args:
         lat1 (array_like): Latitudes of the first set of points
@@ -74,9 +74,9 @@ def bearing(
     dlon = lon2_rad - lon1_rad
 
     theta = np.arctan2(
-        np.sin(dlon) * np.cos(lat2_rad),
         np.cos(lat1) * np.sin(lat2_rad)
         - np.sin(lat1_rad) * np.cos(lat2_rad) * np.cos(dlon),
+        np.sin(dlon) * np.cos(lat2_rad),
     )
 
     return theta

tests/haversine_tests.py
@@ -41,11 +41,11 @@ class haversine_tests(unittest.TestCase):
         )
 
     def test_bearing(self):
-        self.assertTrue(np.isclose(bearing(0, 0, 0.1, 0), 0))
+        self.assertTrue(np.isclose(bearing(0, 0, 0, 0.1), 0))
         self.assertTrue(np.isclose(bearing(0, 0, 0.1, 0.1), np.pi / 4))
-        self.assertTrue(np.isclose(bearing(0, 0, 0, 0.1), np.pi / 2))
-        self.assertTrue(np.isclose(bearing(0, 0, -0.1, 0.1), 3 / 4 * np.pi))
-        self.assertTrue(np.isclose(bearing(0, 0, -0.1, 0), np.pi))
+        self.assertTrue(np.isclose(bearing(0, 0, 0.1, 0), np.pi / 2))
+        self.assertTrue(np.isclose(bearing(0, 0, 0.1, -0.1), 3 / 4 * np.pi))
+        self.assertTrue(np.isclose(bearing(0, 0, 0, -0.1), np.pi))
         self.assertTrue(np.isclose(bearing(0, 0, -0.1, -0.1), -3 / 4 * np.pi))
-        self.assertTrue(np.isclose(bearing(0, 0, 0, -0.1), -np.pi / 2))
-        self.assertTrue(np.isclose(bearing(0, 0, 0.1, -0.1), -np.pi / 4))
+        self.assertTrue(np.isclose(bearing(0, 0, -0.1, 0), -np.pi / 2))
+        self.assertTrue(np.isclose(bearing(0, 0, -0.1, 0.1), -np.pi / 4))