2fbdc1df35dd3331df4f346537dd55006f6245c2
Philippe Miron <philippemiron@gmail.com>
2023-02-18 15:38:03 -0500

    add sphere to the docs

--- clouddrift/sphere.py
+++ clouddrift/sphere.py
@@ -2,20 +2,20 @@ import numpy as np
 from typing import Optional
 
 
-def rot(x: np.ndarray):
+def rot(x: np.ndarray) -> np.ndarray:
     """Complex-valued rotation"""
     x = np.mod(x + np.pi, 2 * np.pi) - np.pi  # convert to [-pi, pi]
     return np.exp(1j * x)
 
 
-def recast_longitude(lon: np.ndarray, lon0: Optional[float] = -180):
+def recast_longitude(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarray:
     """
     Recast (convert) longitude values to a selected range of 360 degrees starting from lon0.
 
     As an example, with:
-    - lon0 = -180, longitude values are converted to range [-180, 180]
-    - lon = 0, longitude values are converted to range [0, 360]
-    - lon = 20, longitude values are converted to range [20, 380]
+        - lon0 = -180, longitude values are converted to range [-180, 180]
+        - lon = 0, longitude values are converted to range [0, 360]
+        - lon = 20, longitude values are converted to range [20, 380]
 
     Args:
         lon (array_like): An N-d array of x-positions (longitude in degrees)
