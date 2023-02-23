cad60141a569c9578280a5b95ce8dc3370a37b31
Philippe Miron <philippemiron@gmail.com>
2023-02-23 14:04:59 -0500

    recast longitude (#124)
    * add sphere to the docs
    
    * fix docstring
    
    * converts scalar to numpy array
    
    * Dosctring for rot; tidy up recast_longitude docstring
    
    * modify fun, added lon180/360
    
    * remove rot
    
    * simplify func
    
    * lint
    
    * Expand and tidy up docstrings
    
    ---------
    
    Co-authored-by: milancurcic <caomaco@gmail.com>
    Co-authored-by: Philippe Miron <philippe.miron@dtn.com>

--- clouddrift/sphere.py
+++ clouddrift/sphere.py
@@ -2,36 +2,8 @@ import numpy as np
 from typing import Optional
 
 
-def rot(x: np.ndarray) -> np.ndarray:
-    """Rotate and return the complex phase of x.
-
-    Parameters
-    ----------
-    x : np.ndarray
-        An N-d array of angles in radians
-
-    Returns
-    -------
-    np.ndarray
-        Rotated and complex phase of x
-
-    Examples
-    --------
-
-    .. code-block:: python
-
-        rot(0) # (1+0j)
-        rot(np.pi / 2) # approx. (0+1j)
-        rot(np.pi) # approx. (-1+0j)
-        rot(3 * np.pi / 2) # approx. (0-1j)
-    """
-    x = np.mod(x + np.pi, 2 * np.pi) - np.pi  # convert to [-pi, pi]
-    return np.exp(1j * x)
-
-
-def recast_longitude(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarray:
-    """Recast (convert) longitude values to a selected range of 360 degrees
-    starting from ``lon0``.
+def recast_lon(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarray:
+    """Recast (convert) longitude values to a selected range of 360 degrees starting from ``lon0``.
 
     Parameters
     ----------
@@ -48,38 +20,88 @@ def recast_longitude(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarra
     Examples
     --------
 
-    By default, ``recast_longitude`` converts longitude values to the range
+    By default, ``recast_lon`` converts longitude values to the range
     `[-180, 180]`:
 
-    .. code-block:: python
-
-        recast_longitude(200) # -160
+    >>> recast_lon(200)
+    -160
 
     The range of the output longitude is controlled by ``lon0``.
     For example, with ``lon0 = 0``, the longitude values are converted to the
     range `[0, 360]`.
 
-    .. code-block:: python
-
-        recast_longitude(200, -180) # -160
+    >>> recast_lon(200, -180)
+    -160
 
     With ``lon0 = 20``, longitude values are converted to range `[20, 380]`,
     which can be useful to avoid cutting the major ocean basins.
 
-    .. code-block:: python
+    >>> recast_lon(10, 20)
+    370
 
-        recast_longitude(10, 20) # 370
-
+    See Also
+    --------
+    :func:`recast_lon360`, :func:`recast_lon180`
     """
-
     if np.isscalar(lon):
         lon = np.array([lon])
 
-    return (
-        np.mod(
-            np.divide(360, 2 * np.pi)
-            * np.unwrap(np.angle(rot(np.divide(2 * np.pi, 360) * (lon - lon0)))),
-            360,
-        )
-        + lon0
-    )
+    return np.mod(lon - lon0, 360) + lon0
+
+
+def recast_lon360(lon: np.ndarray) -> np.ndarray:
+    """Recast (convert) longitude values to the range `[0, 360]`.
+    This is a convenience wrapper around :func:`recast_lon` with ``lon0 = 0``.
+
+    Parameters
+    ----------
+    lon : np.ndarray
+        An N-d array of longitudes in degrees
+
+    Returns
+    -------
+    np.ndarray
+        Converted longitudes in the range `[0, 360]`
+
+    Examples
+    --------
+    >>> recast_lon360(200)
+    200
+
+    >>> recast_lon360(-200)
+    160
+
+    See Also
+    --------
+    :func:`recast_lon`, :func:`recast_lon180`
+    """
+    return recast_lon(lon, 0)
+
+
+def recast_lon180(lon: np.ndarray) -> np.ndarray:
+    """Recast (convert) longitude values to the range `[-180, 180]`.
+    This is a convenience wrapper around :func:`recast_lon` with ``lon0 = -180``.
+
+    Parameters
+    ----------
+    lon : np.ndarray
+        An N-d array of longitudes in degrees
+
+    Returns
+    -------
+    np.ndarray
+        Converted longitudes in the range `[-180, 180]`
+
+    Examples
+    --------
+    >>> recast_lon180(200)
+    -160
+
+    >>> recast_lon180(-200)
+    160
+
+    See Also
+    --------
+    :func:`recast_lon`, :func:`recast_lon360`
+    """
+    return recast_lon(lon, -180)
--- tests/sphere_tests.py
+++ tests/sphere_tests.py
@@ -1,4 +1,4 @@
-from clouddrift.sphere import recast_longitude
+from clouddrift.sphere import recast_lon, recast_lon180, recast_lon360
 import unittest
 import numpy as np
 
@@ -9,61 +9,56 @@ if __name__ == "__main__":
 
 class recast_longitude_tests(unittest.TestCase):
     def test_same_shape(self):
-        self.assertTrue(recast_longitude(np.array([200])).shape == np.zeros(1).shape)
-        self.assertTrue(
-            recast_longitude(np.array([200, 200])).shape == np.zeros(2).shape
-        )
+        self.assertTrue(recast_lon(np.array([200])).shape == np.zeros(1).shape)
+        self.assertTrue(recast_lon(np.array([200, 200])).shape == np.zeros(2).shape)
         self.assertIsNone(
             np.testing.assert_equal(
-                recast_longitude(np.array([[200.5, -200.5], [200.5, -200.5]])).shape,
+                recast_lon(np.array([[200.5, -200.5], [200.5, -200.5]])).shape,
                 np.zeros((2, 2)).shape,
             )
         )
 
     def test_different_lon0(self):
         self.assertIsNone(
+            np.testing.assert_allclose(recast_lon180(np.array([200])), np.array([-160]))
+        )
+        self.assertIsNone(
+            np.testing.assert_allclose(recast_lon360(np.array([200])), np.array([200]))
+        )
+        self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([200])), np.array([-160])
+                recast_lon(np.array([200]), 220), np.array([560])
             )
         )
         self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([200]), 0), np.array([200])
+                recast_lon(np.array([200]), -200), np.array([-160])
             )
         )
         self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([200]), 220), np.array([560])
-            )
-        )
-        self.assertIsNone(
-            np.testing.assert_allclose(
-                recast_longitude(np.array([200]), -200), np.array([-160])
+                recast_lon(np.array([200]), -200), np.array([-160])
             )
         )
 
     def test_range_close(self):
         self.assertIsNone(
-            np.testing.assert_allclose(
-                recast_longitude(np.array([-180])), np.array([-180])
-            )
+            np.testing.assert_allclose(recast_lon(np.array([-180])), np.array([-180]))
         )
         self.assertIsNone(
-            np.testing.assert_allclose(
-                recast_longitude(np.array([0]), 0), np.array([0])
-            )
+            np.testing.assert_allclose(recast_lon(np.array([0]), 0), np.array([0]))
         )
 
         self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([-20]), -20), np.array([-20])
+                recast_lon(np.array([-20]), -20), np.array([-20])
             )
         )
 
     def test_multiple_values(self):
         self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([200, -180, 300])),
+                recast_lon(np.array([200, -180, 300])),
                 np.array([-160, -180, -60]),
             )
         )
@@ -71,13 +66,13 @@ class recast_longitude_tests(unittest.TestCase):
     def test_more_than_360(self):
         self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([400, -400])), np.array([40, -40])
+                recast_lon(np.array([400, -400])), np.array([40, -40])
             )
         )
 
     def test_decimals(self):
         self.assertIsNone(
             np.testing.assert_allclose(
-                recast_longitude(np.array([200.3, -200.2])), np.array([-159.7, 159.8])
+                recast_lon(np.array([200.3, -200.2])), np.array([-159.7, 159.8])
             )
         )
