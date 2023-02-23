158337aece4c53f487345ef9118e1b17ec7e2e93
milancurcic <caomaco@gmail.com>
2023-02-23 14:00:34 -0500

    Expand and tidy up docstrings

--- clouddrift/sphere.py
+++ clouddrift/sphere.py
@@ -23,24 +23,25 @@ def recast_lon(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarray:
     By default, ``recast_lon`` converts longitude values to the range
     `[-180, 180]`:
 
-    .. code-block:: python
-
-        recast_lon(200) # -160
+    >>> recast_lon(200)
+    -160
 
     The range of the output longitude is controlled by ``lon0``.
     For example, with ``lon0 = 0``, the longitude values are converted to the
     range `[0, 360]`.
 
-    .. code-block:: python
-
-        recast_lon(200, -180) # -160
+    >>> recast_lon(200, -180)
+    -160
 
     With ``lon0 = 20``, longitude values are converted to range `[20, 380]`,
     which can be useful to avoid cutting the major ocean basins.
 
-    .. code-block:: python
+    >>> recast_lon(10, 20)
+    370
 
-        recast_lon(10, 20) # 370
+    See Also
+    --------
+    :func:`recast_lon360`, :func:`recast_lon180`
     """
     if np.isscalar(lon):
         lon = np.array([lon])
@@ -49,10 +50,58 @@ def recast_lon(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarray:
 
 
 def recast_lon360(lon: np.ndarray) -> np.ndarray:
-    """Recast (convert) longitude values to the range `[0, 360]`."""
+    """Recast (convert) longitude values to the range `[0, 360]`.
+    This is a convenience wrapper around :func:`recast_lon` with ``lon0 = 0``.
+
+    Parameters
+    ----------
+    lon : np.ndarray
+        An N-d array of longitudes in degrees
+
+    Returns
+    -------
+    np.ndarray
+        Converted longitudes in the range `[0, 360]`
+
+    Examples
+    --------
+    >>> recast_lon360(200)
+    200
+
+    >>> recast_lon360(-200)
+    160
+
+    See Also
+    --------
+    :func:`recast_lon`, :func:`recast_lon180`
+    """
     return recast_lon(lon, 0)
 
 
 def recast_lon180(lon: np.ndarray) -> np.ndarray:
-    """Recast (convert) longitude values to the range `[-180, 180]`."""
+    """Recast (convert) longitude values to the range `[-180, 180]`.
+    This is a convenience wrapper around :func:`recast_lon` with ``lon0 = -180``.
+
+    Parameters
+    ----------
+    lon : np.ndarray
+        An N-d array of longitudes in degrees
+
+    Returns
+    -------
+    np.ndarray
+        Converted longitudes in the range `[-180, 180]`
+
+    Examples
+    --------
+    >>> recast_lon180(200)
+    -160
+
+    >>> recast_lon180(-200)
+    160
+
+    See Also
+    --------
+    :func:`recast_lon`, :func:`recast_lon360`
+    """
     return recast_lon(lon, -180)
