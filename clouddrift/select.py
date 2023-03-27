4f017ec2e4d2fc440125f6d9fc9404467333b7a7
Philippe Miron <philippe.miron@dtn.com>
2023-03-27 14:26:23 -0400

    resolve most of the comments

--- clouddrift/select.py
+++ clouddrift/select.py
@@ -1,27 +1,53 @@
 import xarray as xr
 import numpy as np
 import warnings
+from typing import Union
 
 
-def mask_var(var: xr.DataArray, value):
+def mask_var(var: xr.DataArray, criterion: Union[tuple, list, bool, float, int]) -> xr.DataArray:
     """Return the mask of a subset of the data matching a test criterion.
 
-    Args:
-        var (xr.DataArray): ak.Array
-        value: tuple, list or scalar defining a test criterion
+    Parameters
+    ----------
+    var : xr.DataArray
+        DataArray to be subset by the criterion
+    criterion : Union[tuple, list, bool, float, int]
+        The criterion can take three forms:
+        - tuple: (min, max) defining a range
+        - list: [value1, value2, valueN] defining multiples values
+        - scalar: value defining a single value
 
-    Returns:
-        mask (xr.DataArray): where values corresponding to the criterion are True
+    Examples
+    --------
+    >>> x = xr.DataArray(data=np.arange(0, 5))
+    >>> mask_var(x, (2, 4))
+    <xarray.DataArray (dim_0: 5)> 
+    array([False, False,  True,  True,  True])
+    Dimensions without coordinates: dim_0
+    
+    >>> mask_var(x, [0, 2, 4])
+    <xarray.DataArray (dim_0: 5)>
+    array([ True, False, True,  False, True])
+    Dimensions without coordinates: dim_0
+
+    >>> mask_var(x, 4)
+    <xarray.DataArray (dim_0: 5)>
+    array([False, False, False,  True, False])
+    Dimensions without coordinates: dim_0
+
+    Returns
+    -------
+    mask : xr.DataArray
+        The mask of the subset of the data matching the criteria
     """
-
-    if isinstance(value, tuple):  # min/max defining range
-        mask = np.logical_and(var >= min(value), var <= max(value))
-    elif isinstance(value, list):  # select multiples
+    if isinstance(criterion, tuple):  # min/max defining range
+        mask = np.logical_and(var >= criterion[0], var <= criterion[1])
+    elif isinstance(criterion, list):  # select multiple values
         mask = xr.zeros_like(var)
-        for v in value:
+        for v in criterion:
             mask = np.logical_or(mask, var == v)
-    else:  # select one
-        mask = var == value
+    else:  # select one specific value
+        mask = var == criterion
     return mask
 
 
@@ -30,27 +56,37 @@ def subset(ds: xr.Dataset, criteria: dict) -> xr.Dataset:
     a variable to subset is assigned to either a range (valuemin, valuemax), a list [value1, value2, valueN],
     or a single value.
 
-    Args:
-        ds (xr.Dataset): dataset
-        criteria (dict): dictionary containing the variables and the ranges/values to retrieve
+    Parameters
+    ----------
+    ds : xr.Dataset
+        Lagrangian dataset stored in two-dimensional or ragged array format
+    criteria : dict
+        dictionary containing the variables and the ranges/values to subset
 
-    Returns:
-        ds_subset: subset xr.Dataset
+    Returns
+    -------
+    xr.Dataset
+        subset Dataset matching the criterion(a)
 
-    Usage:
-        Operation can be combined, and any data or metadata variables part of the Dataset can
-        be used as a criterion. Criterion can be defined using three datatypes:
+    Examples
+    --------
+    Criteria are combined on any data or metadata variables part of the Dataset.
 
-        Tuple to subset between a range of values:
-        >>> subset(ds, {"lon": (min_lon, max_lon), "lat": (min_lat, max_lat)})  # extract a region
-        >>> subset(ds, {"time": (min_time, max_time)})  # extract a temporal range
+    To subset between a range of values:
+    >>> subset(ds, {"lon": (min_lon, max_lon), "lat": (min_lat, max_lat)})
+    >>> subset(ds, {"time": (min_time, max_time)})
 
-        A list to select multiples values.
-        >>> subset(ds, {"ID": [1, 2, 3]})  # different IDs
+    To select multiples values:
+    >>> subset(ds, {"ID": [1, 2, 3]})
 
-        A scalar for selecting a specific value.
-        >>> subset(ds, {"drogue_status": True})  # extract segment of trajectory with drogue
-    """
+    To select a specific value:
+    >>> subset(ds, {"drogue_status": True})
+
+    Raises
+    ------
+    ValueError
+        If one of the variable in a criterion is not found in the Dataset
+    """    
     mask_traj = xr.DataArray(data=np.ones(ds.dims["traj"], dtype="bool"), dims=["traj"])
     mask_obs = xr.DataArray(data=np.ones(ds.dims["obs"], dtype="bool"), dims=["obs"])
 
@@ -74,7 +110,7 @@ def subset(ds: xr.Dataset, criteria: dict) -> xr.Dataset:
     )
 
     if not any(mask_traj):
-        warnings.warn("Empty set.")
+        warnings.warn("No data matches the criteria; returning an empty dataset.")
         return xr.Dataset()
     else:
         # update rowsize
