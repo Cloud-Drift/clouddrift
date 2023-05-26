fb3e832ab3ab72414bef8e4e8b7f0ba0a59b01b4
milancurcic <caomaco@gmail.com>
2023-05-26 12:26:44 -0400

    Draft implementation of sphere_to_plane

--- clouddrift/sphere.py
+++ clouddrift/sphere.py
@@ -1,5 +1,6 @@
+from clouddrift import haversine
 import numpy as np
-from typing import Optional
+from typing import Optional, Tuple
 
 
 def recast_lon(lon: np.ndarray, lon0: Optional[float] = -180) -> np.ndarray:
@@ -102,3 +103,44 @@ def recast_lon180(lon: np.ndarray) -> np.ndarray:
     :func:`recast_lon`, :func:`recast_lon360`
     """
     return recast_lon(lon, -180)
+
+
+def sphere_to_plane(
+    lon: np.ndarray, lat: np.ndarray, x_origin: float, y_origin: float
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Convert spherical coordinates to a tangent plane.
+
+    Parameters
+    ----------
+    lat : np.ndarray
+        An N-d array of latitudes in degrees
+    lon : np.ndarray
+        An N-d array of longitudes in degrees
+    x_origin : float
+        x-coordinate of the origin of the tangent plane
+    y_origin : float
+        y-coordinate of the origin of the tangent plane
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        x- and y-coordinates of the tangent plane
+    """
+    x = np.empty_like(lon)
+    y = np.empty_like(lon)
+
+    distances = haversine.distance(
+        lat[..., :-1], lon[..., :-1], lat[..., 1:], lon[..., 1:]
+    )
+    bearings = haversine.bearing(
+        lat[..., :-1], lon[..., :-1], lat[..., 1:], lon[..., 1:]
+    )
+
+    dx = distances * np.cos(bearings)
+    dy = distances * np.sin(bearings)
+
+    x[..., 0], y[..., 0] = x_origin, y_origin
+    x[..., 1:] = np.cumsum(dx, axis=-1)
+    y[..., 1:] = np.cumsum(dy, axis=-1)
+
+    return x, y
