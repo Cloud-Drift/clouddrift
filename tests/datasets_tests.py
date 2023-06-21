10a2461d5c8af3c07f858a3ad893e3f3a8d8616d
Milan Curcic <caomaco@gmail.com>
2023-06-21 15:42:48 -0400

    Implement `datasets.gdp6h` (#192)
    * Update gdp1h docstring
    
    * gdp6h() function and test; expand docstrings for gdp1h and gdp6h
    
    * Bump minor version

--- tests/datasets_tests.py
+++ tests/datasets_tests.py
@@ -10,3 +10,7 @@ class datasets_tests(unittest.TestCase):
     def test_gdp1h_opens(self):
         ds = datasets.gdp1h()
         self.assertTrue(ds)
+
+    def test_gdp6h_opens(self):
+        ds = datasets.gdp6h()
+        self.assertTrue(ds)
