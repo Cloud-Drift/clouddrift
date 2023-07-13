f77e580d7e5d01a71bdca9bd766380c5c1920800
milancurcic <caomaco@gmail.com>
2023-07-13 15:43:54 -0400

    Switch to sphinx-book-theme

--- docs/conf.py
+++ docs/conf.py
@@ -49,9 +49,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = "pydata_sphinx_theme"  # alabaster, sphinx_rtd_theme
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"  # alabaster, sphinx_rtd_theme
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
