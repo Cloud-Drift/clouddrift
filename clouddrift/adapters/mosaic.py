a3deb67b5efddba160a79f424b90c962c83401c3
milancurcic <caomaco@gmail.com>
2023-07-18 10:35:58 -0400

    Downloading data into a DataFrame

--- clouddrift/adapters/mosaic.py
+++ clouddrift/adapters/mosaic.py
@@ -2,8 +2,10 @@
 This module defines functions used to adapt the MOSAiC sea-ice drift dataset as
 a ragged-array dataset.
 """
+from concurrent.futures import ThreadPoolExecutor
 import pandas as pd
 import requests
+from tqdm import tqdm
 import xml.etree.ElementTree as ET
 
 
@@ -16,19 +18,36 @@ def get_metadata() -> str:
     return r.content
 
 
-def get_filenames(xml: str) -> list[str]:
-    """Pass the MOSAiC XML string and return the list of available filenames."""
-    return [
+def get_file_urls(xml: str) -> list[str]:
+    """Pass the MOSAiC XML string and return the list of filenames and URLs."""
+    filenames = [
         tag.text
         for tag in ET.fromstring(xml).findall("./dataset/dataTable/physical/objectName")
     ]
-
-
-def get_file_urls(xml: str) -> list[str]:
-    """Pass the MOSAiC XML string and return the list of URLs for the available files."""
-    return [
+    urls = [
         tag.text
         for tag in ET.fromstring(xml).findall(
             "./dataset/dataTable/physical/distribution/online/url"
         )
     ]
+    return [{"filename": f, "url": u} for f, u in zip(filenames, urls)]
+
+
+def get_dataframe() -> pd.DataFrame:
+    """Get the MOSAiC data as a pandas DataFrame."""
+    xml = get_metadata()
+    urls = [
+        f["url"]
+        for f in get_file_urls(xml)
+        if "site_buoy_summary" not in f["filename"] and "buoy_list" not in f["filename"]
+    ]
+    with ThreadPoolExecutor() as executor:
+        dfs = tqdm(
+            executor.map(pd.read_csv, urls),
+            total=len(urls),
+            desc="Downloading data",
+            ncols=80,
+        )
+    # TODO collect ids from filenames
+    # TODO insert ids as a column
+    return pd.concat(dfs)
