2ad0b0402ffe12b486a048a7843d4dd7603d444f
Shane Elipot <selipot@miami.edu>
2023-07-28 16:17:05 -0400

    augmented tests

--- tests/analytic_tests.py
+++ tests/analytic_tests.py
@@ -3,6 +3,8 @@ from clouddrift.analytic import (
 )
 import numpy as np
 import unittest
+import pandas as pd
+import xarray as xr
 
 if __name__ == "__main__":
     unittest.main()
@@ -10,40 +12,50 @@ if __name__ == "__main__":
 
 def test_anatrans_real_odd(self):
     x = np.random.rand(99)
-    x = x - np.mean(x)
     z = anatrans(x)
-    self.assertTrue(np.allclose(x, np.real(z)))
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z)))
 
 
 def test_anatrans_real_even(self):
     x = np.random.rand(100)
-    x = x - np.mean(x)
     z = anatrans(x)
-    self.assertTrue(np.allclose(x, np.real(z)))
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z)))
 
 
 def test_anatrans_imag_odd(self):
     z = np.random.rand(99) + 1j * np.random.rand(99)
-    z = z - np.mean(z)
     zp = anatrans(z)
     zn = anatrans(np.conj(z))
-    self.assertTrue(np.allclose(z, zp + np.conj(zn)))
+    self.assertTrue(np.allclose(z - np.mean(z), zp + np.conj(zn)))
 
 
 def test_anatrans_imag_even(self):
     z = np.random.rand(100) + 1j * np.random.rand(100)
-    z = z - np.mean(z)
     zp = anatrans(z)
     zn = anatrans(np.conj(z))
-    self.assertTrue(np.allclose(z, zp + np.conj(zn)))
+    self.assertTrue(np.allclose(z-np.mean(z), zp + np.conj(zn)))
 
 
 def test_anatrans_boundary(self):
     x = np.random.rand(99)
-    x = x - np.mean(x)
     z1 = anatrans(x, boundary="mirror")
     z2 = anatrans(x, boundary="zeros")
     z3 = anatrans(x, boundary="periodic")
-    self.assertTrue(np.allclose(x, np.real(z1)))
-    self.assertTrue(np.allclose(x, np.real(z2)))
-    self.assertTrue(np.allclose(x, np.real(z3)))
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z1)))
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z2)))
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z3)))
+
+def test_anatrans_list(self):
+    x = list(np.random.rand(99))
+    z = anatrans(x)
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z)))
+
+def test_anatrans_pandas(self):
+    x = pd.Series(data=np.random.rand(99))
+    z = anatrans(x)
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z)))
+
+def test_anatrans_xarray(self):
+    x = xr.DataArray(data=np.random.rand(99))
+    z = anatrans(x)
+    self.assertTrue(np.allclose(x-np.mean(x), np.real(z)))
