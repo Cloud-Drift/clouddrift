1d6278e9c18ce3d3430e51055a2bb2615e3509ed
Shane Elipot <selipot@miami.edu>
2023-07-28 15:50:29 -0400

    initial commit for analytic transform function

---
+++ clouddrift/analytic.py
@@ -0,0 +1,79 @@
+"""
+This module provides signal processing functions.
+"""
+
+import numpy as np
+from typing import Optional, Tuple, Union
+import xarray as xr
+import pandas as pd
+import warnings
+
+def anatrans(
+        x: Union[list, np.ndarray, xr.DataArray, pd.Series],
+        boundary: Optional[str] = "mirror",
+) -> np.ndarray:
+    """returns the analytic part of a real-valued signal or of a complex-valued 
+    signal. To obtain the anti-analytic part of a complex-valued signal apply anatrans
+    to the conjugate of the input. Anatrans removes the mean of the input signals.
+
+    Parameters
+    ----------
+    x : np.ndarray
+    boundary : str, optional ["mirror", "zeros", "periodic"]
+
+    Returns
+    -------
+    z : np.ndarray
+
+    Examples
+    --------
+
+    To obtain the analytic part of a real-valued signal
+    >>> x = np.random.rand(99)
+    >>> z = anatrans(x)
+
+    To obtain the analytic and anti-analytic parts of a complex-valued signal
+    >>> z = np.random.rand(99)+1j*np.random.rand(99)
+    >>> zp = anatrans(z)
+    >>> zn = anatrans(np.conj(z))
+
+    Raises
+    ------
+    ValueError
+        If ``boundary not in ["mirror", "zeros", "periodic"]``.    
+    """
+    # assume unidimensional input; add dimension option
+    m0 = len(x)
+    
+    # remove mean
+    x = x - np.mean(x) 
+    
+    # apply boundary conditions
+    if boundary == "mirror":
+        x = np.concatenate((np.flip(x),x,np.flip(x)))
+    elif boundary == "zeros":
+        x = np.concatenate((np.zeros_like(x),x,np.zeros_like(x)))
+    elif boundary == "periodic":
+        x = np.concatenate((x,x,x))
+    else:
+        raise ValueError("boundary must be one of 'mirror', 'align', or 'zeros'.")
+    
+    m = len(x)
+
+    if np.isrealobj(x):
+        z = 2*np.fft.fft(x)
+    else:
+        z = np.fft.fft(x)
+
+    if m%2 == 0:
+        z[int(m/2+2)-1:int(m+1)+1] = 0 # zero negative frequencies
+    else:
+        z[int((m+3)/2)-1:int(m+1)+1] = 0 # zero negative frequencies
+    
+    # inverse Fourier transform
+    z = np.fft.ifft(z)
+    
+    # return central part
+    z = z[int(m0+1)-1:int(2*m0+1)-1]
+    
+    return z
---
+++ tests/analytic_tests.py
@@ -0,0 +1,44 @@
+from clouddrift.analytic import (
+    anatrans,
+)
+import numpy as np
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
+
+def test_anatrans_real_odd(self):
+        x = np.random.rand(99)
+        x = x-np.mean(x)
+        z = anatrans(x)
+        self.assertTrue(np.allclose(x, np.real(z)))
+
+def test_anatrans_real_even(self):
+        x = np.random.rand(100)
+        x = x-np.mean(x)
+        z = anatrans(x)
+        self.assertTrue(np.allclose(x, np.real(z)))
+
+def test_anatrans_imag_odd(self):
+        z = np.random.rand(99)+1j*np.random.rand(99)
+        z = z-np.mean(z)
+        zp = anatrans(z)
+        zn = anatrans(np.conj(z))
+        self.assertTrue(np.allclose(z, zp+np.conj(zn)))
+
+def test_anatrans_imag_even(self):
+        z = np.random.rand(100)+1j*np.random.rand(100)
+        z = z-np.mean(z)
+        zp = anatrans(z)
+        zn = anatrans(np.conj(z))
+        self.assertTrue(np.allclose(z, zp+np.conj(zn)))
+
+def test_anatrans_boundary(self):
+        x = np.random.rand(99)
+        x = x-np.mean(x)
+        z1 = anatrans(x,boundary='mirror')
+        z2 = anatrans(x,boundary='zeros')
+        z3 = anatrans(x,boundary='periodic')                
+        self.assertTrue(np.allclose(x, np.real(z1)))
+        self.assertTrue(np.allclose(x, np.real(z2)))
+        self.assertTrue(np.allclose(x, np.real(z3)))
