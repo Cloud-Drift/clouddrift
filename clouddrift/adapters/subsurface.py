be74acab2ffa6d1b35afc4f9eda4b94a1a8096bd
Philippe Miron <philippe.miron@dtn.com>
2023-11-13 12:27:30 -0500

    more fixes

--- clouddrift/adapters/subsurface.py
+++ clouddrift/adapters/subsurface.py
@@ -10,18 +10,20 @@ Example
 >>> ds = subsurface.to_xarray()
 """
 
-from clouddrift.adapters.gdp import cut_str
 import scipy.io
 import urllib.request
 import os
 import tempfile
 import xarray as xr
 import numpy as np
+import pandas as pd
 import warnings
+from datetime import datetime
 
 SUBSURFACE_FLOAT_DATA_URL = (
     "https://www.aoml.noaa.gov/phod/float_traj/files/allFloats_12122017.mat"
 )
+SUBSURFACE_FLOAT_VERSION = f"December 2017 (version 2)"
 SUBSURFACE_FLOAT_TMP_PATH = os.path.join(
     tempfile.gettempdir(), "clouddrift", "subsurface_float"
 )
@@ -61,7 +63,7 @@ def to_xarray(
 
     metadata = {}
     for var in meta_variables:
-        metadata[var] = source_data[var].flatten()
+        metadata[var] = np.array([v.flatten()[0] for v in source_data[var].flatten()])
 
     # data
     data_variables = ["dtnum", "lon", "lat", "p", "t", "u", "v"]
@@ -77,33 +79,92 @@ def to_xarray(
     # use those indices to retrieve one value per experiment
     _, indices_exp = np.unique(metadata["indexExp"], return_index=True)
 
-    # todo 2 to fix netcdf export maybe...
-    # "experiment_list": (["exp"], cut_str(metadata["expList"], 20)),
-    # "experiment_name": (["exp"], cut_str(metadata["expName"][indices_exp], 20)),
-    # "experiment_org": (["exp"], cut_str(metadata["expOrg"][indices_exp], 20)),
-    # "experiment_pi": (["exp"], cut_str(metadata["expPI"][indices_exp]), 20),
+    origin_datenum = 719529  # Unix epoch start (1970-01-01)
 
     ds = xr.Dataset(
         {
-            "experiment_list": (["exp"], metadata["expList"]),
-            "experiment_name": (["exp"], metadata["expName"][indices_exp]),
-            "experiment_org": (["exp"], metadata["expOrg"][indices_exp]),
-            "experiment_pi": (["exp"], metadata["expPI"][indices_exp]),
-            "index_exp": (["traj"], metadata["indexExp"]),
-            "float_type": (["traj"], metadata["fltType"]),
+            "expList": (["exp"], metadata["expList"]),
+            "expName": (["exp"], metadata["expName"][indices_exp]),
+            "expOrg": (["exp"], metadata["expOrg"][indices_exp]),
+            "expPI": (["exp"], metadata["expPI"][indices_exp]),
+            "indexExp": (["traj"], metadata["indexExp"]),
+            "fltType": (["traj"], metadata["fltType"]),
             "id": (["traj"], metadata["indexFlt"]),
             "rowsize": (["traj"], rowsize),
-            "datenum": (["obs"], data["dtnum"]),
+            "time": (
+                ["obs"],
+                pd.to_datetime(data["dtnum"] - origin_datenum, unit="D"),
+            ),
             "lon": (["obs"], data["lon"]),
             "lat": (["obs"], data["lat"]),
-            "pressure": (["obs"], data["p"]),
-            "temperature": (["obs"], data["t"]),
-            "ve": (["obs"], data["u"]),
-            "vn": (["obs"], data["v"]),
+            "p": (["obs"], data["p"]),
+            "t": (["obs"], data["t"]),
+            "u": (["obs"], data["u"]),
+            "v": (["obs"], data["v"]),
         }
     )
 
+    # define attributes
+    vars_attrs = {
+        "expList": {
+            "long_name": "Experiment list",
+            "units": "-",
+        },
+        "expName": {
+            "long_name": "Experiment name",
+            "units": "-",
+        },
+        "expOrg": {
+            "long_name": "Experiment organization",
+            "units": "-",
+        },
+        "expPI": {
+            "long_name": "Experiment principal investigator",
+            "units": "-",
+        },
+        "indexExp": {
+            "long_name": "Experiment index number",
+            "units": "-",
+            "comment": "The index matches the float with its experiment metadata",
+        },
+        "fltType": {
+            "long_name": "Float type",
+            "units": "-",
+        },
+        "id": {"long_name": "Float ID", "units": "-"},
+        "lon": {"long_name": "Longitude", "units": "degrees_east"},
+        "lat": {"long_name": "Latitude", "units": "degrees_north"},
+        "rowsize": {
+            "long_name": "Number of observations per trajectory",
+            "sample_dimension": "obs",
+            "units": "-",
+        },
+        "u": {"long_name": "Eastward velocity", "units": "m/s"},
+        "v": {"long_name": "Northward velocity", "units": "m/s"},
+        "t": {"long_name": "Temperature", "units": "Celsius"},
+        "p": {"long_name": "Pressure", "units": "Millibar"},
+    }
+
+    # global attributes
+    attrs = {
+        "title": "Subsurface float trajectories dataset",
+        "history": SUBSURFACE_FLOAT_VERSION,
+        "date_created": datetime.now().isoformat(),
+        "publisher_name": "WOCE Subsurface Float Data Assembly Center and NOAA AOML",
+        "publisher_url": "https://www.aoml.noaa.gov/phod/float_traj/data.php",
+        "licence": "freely available",
+        "acknowledgement": f"Maintained by Andree Ramsey and Heather Furey from the Woods Hole Oceanographic Institution",
+    }
+
+    # set attributes
+    for var in vars_attrs.keys():
+        if var in ds.keys():
+            ds[var].attrs = vars_attrs[var]
+        else:
+            warnings.warn(f"Variable {var} not found in upstream data; skipping.")
+    ds.attrs = attrs
+
     # set coordinates
-    ds = ds.set_coords(["datenum", "ids"])
+    ds = ds.set_coords(["time", "id"])
 
     return ds
