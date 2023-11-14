90a4854b6a872e7732c22372f8d0a094665f6f54
selipot <selipot@miami.edu>
2023-11-14 13:56:40 -0500

    docstring edits

--- clouddrift/adapters/subsurface_floats.py
+++ clouddrift/adapters/subsurface_floats.py
@@ -1,7 +1,7 @@
 """
-This module defines functions used to adapt the subsurface float trajectories as
-a ragged-array dataset. The dataset contains 2193 trajectories of SOFAR, APEX, and
-RAFOS floats from 52 experiments across the world between 1989 and 2015.
+This module defines functions to adapt as a ragged-array dataset a collection of data 
+from 2193 trajectories of SOFAR, APEX, and RAFOS subsurface floats from 52 experiments 
+across the world between 1989 and 2015.
 
 The dataset is hosted at https://www.aoml.noaa.gov/phod/float_traj/index.php
 
