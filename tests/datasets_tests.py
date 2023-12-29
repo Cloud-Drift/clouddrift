4cf261ab7ea281475fa6a5bf32dd8d3ee51b271f
Philippe Miron <philippe.miron@dtn.com>
2023-12-29 18:50:10 -0300

    use context manager to close datasets

--- tests/datasets_tests.py
+++ tests/datasets_tests.py
@@ -10,45 +10,45 @@ if __name__ == "__main__":
 
 class datasets_tests(unittest.TestCase):
     def test_gdp1h(self):
-        ds = datasets.gdp1h()
-        self.assertTrue(ds)
+        with datasets.gdp1h() as ds:
+            self.assertTrue(ds)
 
     def test_gdp6h(self):
-        ds = datasets.gdp6h()
-        self.assertTrue(ds)
+        with datasets.gdp6h() as ds:
+            self.assertTrue(ds)
 
     def test_glad(self):
-        ds = datasets.glad()
-        self.assertTrue(ds)
+        with datasets.glad() as ds:
+            self.assertTrue(ds)
 
     def test_glad_dims_coords(self):
-        ds = datasets.glad()
-        self.assertTrue(len(ds.sizes) == 2)
-        self.assertTrue("obs" in ds.dims)
-        self.assertTrue("traj" in ds.dims)
-        self.assertTrue(len(ds.coords) == 2)
-        self.assertTrue("time" in ds.coords)
-        self.assertTrue("id" in ds.coords)
+        with datasets.glad() as ds:
+            self.assertTrue(len(ds.sizes) == 2)
+            self.assertTrue("obs" in ds.dims)
+            self.assertTrue("traj" in ds.dims)
+            self.assertTrue(len(ds.coords) == 2)
+            self.assertTrue("time" in ds.coords)
+            self.assertTrue("id" in ds.coords)
 
     def test_glad_subset_and_apply_ragged_work(self):
-        ds = datasets.glad()
-        ds_sub = subset(ds, {"id": ["CARTHE_001", "CARTHE_002"]}, id_var_name="id")
-        self.assertTrue(ds_sub)
-        mean_lon = apply_ragged(np.mean, [ds_sub.longitude], ds_sub.rowsize)
-        self.assertTrue(mean_lon.size == 2)
+        with datasets.glad() as ds:
+            ds_sub = subset(ds, {"id": ["CARTHE_001", "CARTHE_002"]}, id_var_name="id")
+            self.assertTrue(ds_sub)
+            mean_lon = apply_ragged(np.mean, [ds_sub.longitude], ds_sub.rowsize)
+            self.assertTrue(mean_lon.size == 2)
 
     def test_spotters_opens(self):
-        ds = datasets.spotters()
-        self.assertTrue(ds)
+        with datasets.spotters() as ds:
+            self.assertTrue(ds)
 
     def test_subsurface_floats_opens(self):
-        ds = datasets.subsurface_floats()
-        self.assertTrue(ds)
+        with datasets.subsurface_floats() as ds:
+            self.assertTrue(ds)
 
     def test_andro_opens(self):
-        ds = datasets.andro()
-        self.assertTrue(ds)
+        with datasets.andro() as ds:
+            self.assertTrue(ds)
 
     def test_yomaha_opens(self):
-        ds = datasets.yomaha()
-        self.assertTrue(ds)
+        with datasets.yomaha() as ds:
+            self.assertTrue(ds)
