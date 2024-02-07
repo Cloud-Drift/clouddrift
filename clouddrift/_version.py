e66ab147b1ce61e05d9b1586d4e7e49b9791ff06
Kevin Santana <kevinsantana11@gmail.com>
2024-02-06 23:45:05 -0500

    Downloading clouddrift as a dependency creates naming collision with local package files

--- clouddrift/_version.py
+++ clouddrift/_version.py
@@ -1,3 +1,14 @@
+import logging
+
 import pkg_resources
 
-version = pkg_resources.get_distribution("clouddrift").version
+_logger = logging.getLogger(__name__)
+
+try:
+    version = pkg_resources.get_distribution("clouddrift").version
+except pkg_resources.DistributionNotFound:
+    # Handle case when developing on a local copy of the library rather than using an 
+    # installed copy from a distribution.
+    version = "9999"
+    _logger.info(f"Cannot determine package version using {version}")
+
