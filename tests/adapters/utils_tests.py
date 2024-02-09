3b3a00f322e14e91d79b5e2d69247dfd8f337437
Kevin Santana <kevinsantana11@gmail.com>
2024-02-09 09:52:32 -0500

    add more tests for download_with_progress utility function

--- tests/adapters/utils_tests.py
+++ tests/adapters/utils_tests.py
@@ -1,65 +1,125 @@
 import unittest
 from datetime import datetime, timedelta
-from unittest.mock import Mock, patch
-
-from requests import Timeout
-from tenacity import retry, retry_if_exception, stop_after_attempt, wait_fixed
+from unittest.mock import Mock, mock_open, patch
 
 from clouddrift.adapters import utils
 from tests.adapters.utils import MultiPatcher
 
 
 class utils_tests(unittest.TestCase):
-    def test_only_download_when_updated(self):
+    head_response_mock: Mock
+    get_response_mock: Mock
+    requests_mock: Mock
+    open_mock: Mock
+    bar_mock: Mock
+
+    def setUp(self) -> None:
+        self.head_response_mock = Mock()
+        self.head_response_mock.headers = {"Last-Modified": "some-date-str"}
+        self.head_response_mock.__enter__ = Mock(return_value=self.head_response_mock)
+        self.head_response_mock.__exit__ = Mock()
+
+        self.get_response_mock = Mock()
+        self.get_response_mock.headers = dict()
+        self.get_response_mock.iter_content = Mock(return_value=["a", "b", "c"])
+
+        self.requests_mock = Mock()
+        self.requests_mock.head = Mock(return_value=self.head_response_mock)
+        self.requests_mock.get = Mock(return_value=self.get_response_mock)
+
+        self.open_mock = mock_open()
+
+        self.bar_mock = Mock()
+        self.bar_mock.update = Mock()
+
+    def test_forgo_download_no_update(self):
         """
-        Only download files from remote server if local file doesn't exist or the file has been updated.
+        Don't download from the server if we have the latest version of the file.
         """
-        now = datetime.now().timestamp()
-        response_mock = Mock()
-        response_mock.headers = Mock(return_value={"Last-Modified": now})
+
+        now = datetime.now()
+        datetime_mock = Mock()
+        datetime_mock.strptime = Mock(return_value=now)
 
         with MultiPatcher(
             [
-                patch("clouddrift.adapters.utils.requests.get", Mock()),
+                patch("clouddrift.adapters.utils.datetime", datetime_mock),
                 patch(
-                    "clouddrift.adapters.utils.requests.head",
-                    Mock(return_value=response_mock),
+                    "clouddrift.adapters.utils.os.path.getmtime",
+                    Mock(return_value=now.timestamp()),
                 ),
+                patch("clouddrift.adapters.utils.requests", self.requests_mock),
+                patch("clouddrift.adapters.utils.open", self.open_mock),
+                patch(
+                    "clouddrift.adapters.utils.os.path.exists", Mock(return_value=True)
+                ),
+            ]
+        ) as _:
+            utils._download_with_progress(
+                "some.url.com", "./some/path/existing-file.nc", 0, False, lambda x: x
+            )
+            self.requests_mock.get.assert_not_called()
+
+    def test_download_new_update(self):
+        """
+        Download the file from the server if there is an update to it remotely.
+        """
+        now = datetime.now()
+        datetime_mock = Mock()
+        datetime_mock.strptime = Mock(return_value=now)
+        with MultiPatcher(
+            [
+                patch("clouddrift.adapters.utils.datetime", datetime_mock),
+                patch("clouddrift.adapters.utils.requests", self.requests_mock),
+                patch("clouddrift.adapters.utils.open", self.open_mock),
                 patch(
                     "clouddrift.adapters.utils.os.path.exists", Mock(return_value=True)
                 ),
                 patch(
-                    "clouddrift.adapters.utils.os.path.getmtime", Mock(return_value=now)
+                    "clouddrift.adapters.utils.os.path.getmtime",
+                    Mock(return_value=(now - timedelta(days=1)).timestamp()),
                 ),
             ]
-        ) as mocks:
-            utils.download_with_progress(
-                [("some.url.com", "./some/path/existing-file.nc", None)]
+        ) as _:
+            utils._download_with_progress(
+                "some.url.com", "./some/path/existing-file.nc", 0, False, lambda x: x
             )
-            mocks[0].assert_not_called()
-            mocks[1].assert_called()
+            self.requests_mock.get.assert_called()
 
-    def test_retry_mechanism(self):
+    def test_progress_mechanism_enabled(self):
         """
-        Ensure retry mechanism works.
+        Ensure we show progress when user enables the feature
         """
         with MultiPatcher(
             [
                 patch(
-                    "clouddrift.adapters.utils.os.path.exists", Mock(return_value=False)
+                    "clouddrift.adapters.utils.tqdm", Mock(return_value=self.bar_mock)
                 ),
-                patch(
-                    "clouddrift.adapters.utils.requests.get",
-                    Mock(side_effect=Timeout()),
-                ),
+                patch("clouddrift.adapters.utils.open", self.open_mock),
+                patch("clouddrift.adapters.utils.requests", self.requests_mock),
             ]
-        ) as mocks:
-            utils.download_with_progress(
-                [("some.url.com", "./some/path/existing-file.nc", None)],
-                custom_retry_protocol=retry(
-                    wait=wait_fixed(timedelta(seconds=1)),
-                    stop=stop_after_attempt(3),
-                    retry=retry_if_exception(lambda ex: isinstance(ex, (Timeout,))),
-                ),
+        ) as mock:
+            utils._download_with_progress(
+                "some.url.com", "./some/path/existing-file.nc", 0, True, lambda x: x
             )
-            assert mocks[1].call_count == 3
+            mock[0].assert_called()
+            self.bar_mock.update.assert_called()
+
+    def test_progress_mechanism_disabled(self):
+        """
+        Ensure we don't show progress when user disables the feature
+        """
+        with MultiPatcher(
+            [
+                patch(
+                    "clouddrift.adapters.utils.tqdm", Mock(return_value=self.bar_mock)
+                ),
+                patch("clouddrift.adapters.utils.open", self.open_mock),
+                patch("clouddrift.adapters.utils.requests", self.requests_mock),
+            ]
+        ) as mock:
+            utils._download_with_progress(
+                "some.url.com", "./some/path/existing-file.nc", 0, False, lambda x: x
+            )
+            mock[0].assert_not_called()
+            self.bar_mock.update.assert_not_called()
