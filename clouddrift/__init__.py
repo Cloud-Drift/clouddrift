27fb108a9a9924a60debb3bbeca5b9f1c06aafa2
Shane Elipot <selipot@miami.edu>
2024-03-19 14:28:21 -0400

    ruffed __init__.py

--- clouddrift/__init__.py
+++ clouddrift/__init__.py
@@ -10,9 +10,9 @@ import clouddrift.plotting as plotting
 import clouddrift.ragged as ragged
 import clouddrift.signal as signal
 import clouddrift.sphere as sphere
+import clouddrift.transfer as transfer
 import clouddrift.wavelet as wavelet
 from clouddrift.raggedarray import RaggedArray
-import clouddrift.transfer as transfer
 
 __all__ = [
     "RaggedArray",
