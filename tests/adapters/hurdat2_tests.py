76df719a9c5275ce0dfecec4f3523f8dd3e7753f
Kevin Santana <kevinsantana11@gmail.com>
2024-03-19 22:00:19 -0400

    update tests

--- tests/adapters/hurdat2_tests.py
+++ tests/adapters/hurdat2_tests.py
@@ -4,10 +4,10 @@ from clouddrift.adapters import hurdat2
 
 
 class hurdat2_tests(unittest.TestCase):
-    def test_basin_selection(self):
+    def test_basin_selection(self) -> None:
         with self.subTest("Select Atlantic Basin"):
             download_requests = hurdat2._get_download_requests(
-                hurdat2.BasinOption.ATLANTIC
+                hurdat2.BasinOption.ATLANTIC, "some_path"
             )
             url, _, _ = download_requests[0]
 
@@ -16,7 +16,7 @@ class hurdat2_tests(unittest.TestCase):
 
         with self.subTest("Select Pacific Basin"):
             download_requests = hurdat2._get_download_requests(
-                hurdat2.BasinOption.PACIFIC
+                hurdat2.BasinOption.PACIFIC, "some_path"
             )
             url, _, _ = download_requests[0]
 
@@ -24,19 +24,9 @@ class hurdat2_tests(unittest.TestCase):
             assert url == hurdat2._PACIFIC_BASIN_URL
 
         with self.subTest("Select Both Option"):
-            download_requests = hurdat2._get_download_requests(hurdat2.BasinOption.BOTH)
+            download_requests = hurdat2._get_download_requests(hurdat2.BasinOption.BOTH, "some_path")
             atlantic_url, _, _ = download_requests[0]
             pacific_url, _, _ = download_requests[1]
-
-            assert len(download_requests) == 2
-            assert atlantic_url == hurdat2._ATLANTIC_BASIN_URL
-            assert pacific_url == hurdat2._PACIFIC_BASIN_URL
-
-        with self.subTest("Select Both Explicitly"):
-            download_requests = hurdat2._get_download_requests(
-                hurdat2.BasinOption.PACIFIC | hurdat2.BasinOption.ATLANTIC
-            )
-            url, _, _ = download_requests[0]
 
             assert len(download_requests) == 2
             assert atlantic_url == hurdat2._ATLANTIC_BASIN_URL
