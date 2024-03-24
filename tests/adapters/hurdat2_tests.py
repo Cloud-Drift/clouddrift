f7b919b654acfd19fa7f1932deea7051831735a0
Kevin Santana <kevinsantana11@gmail.com>
2024-03-23 20:48:16 -0400

    test changes for simplifcation of ocean selection

--- tests/adapters/hurdat2_tests.py
+++ tests/adapters/hurdat2_tests.py
@@ -7,7 +7,7 @@ class hurdat2_tests(unittest.TestCase):
     def test_basin_selection(self) -> None:
         with self.subTest("Select Atlantic Basin"):
             download_requests = hurdat2._get_download_requests(
-                hurdat2.BasinOption.ATLANTIC, "some_path"
+                "atlantic", "some_path"
             )
             url, _, _ = download_requests[0]
 
@@ -16,7 +16,7 @@ class hurdat2_tests(unittest.TestCase):
 
         with self.subTest("Select Pacific Basin"):
             download_requests = hurdat2._get_download_requests(
-                hurdat2.BasinOption.PACIFIC, "some_path"
+                "pacific", "some_path"
             )
             url, _, _ = download_requests[0]
 
@@ -24,7 +24,7 @@ class hurdat2_tests(unittest.TestCase):
             assert url == hurdat2._PACIFIC_BASIN_URL
 
         with self.subTest("Select Both Option"):
-            download_requests = hurdat2._get_download_requests(hurdat2.BasinOption.BOTH, "some_path")
+            download_requests = hurdat2._get_download_requests("both", "some_path")
             atlantic_url, _, _ = download_requests[0]
             pacific_url, _, _ = download_requests[1]
 
