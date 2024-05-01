f263044935ae09f1268c155aae6eba0ff4d10865
Kevin Santana <kevinsantana11@gmail.com>
2024-05-01 08:56:08 -0400

    simplify adapter

--- clouddrift/adapters/gdp/rawfiles.py
+++ clouddrift/adapters/gdp/rawfiles.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
 import datetime
-import gzip
 import logging
-import math
 import os
 import tempfile
-from abc import abstractmethod
-from collections import defaultdict
-from dataclasses import dataclass, field
-from typing import Literal, override
+from dataclasses import dataclass
+from typing import Callable, Literal
 
+import numpy as np
+import pandas as pd
+import xarray as xr
 from tqdm import tqdm
 
 from clouddrift.adapters.gdp import get_gdp_metadata
@@ -30,194 +29,113 @@ _RecordKind = Literal["position"] | Literal["sensor"] | Literal["raw"]
 _logger = logging.getLogger(__name__)
 
 
-@dataclass
-class DrifterMetadata:
-    id: int = field(
-        metadata=dict()
-    )
-    wmoNumber: int = field(
-        metadata=dict()
-    )
-    programNumber: int = field(
-        metadata=dict()
-    )
-    buoyType: str = field(
-        metadata=dict()
-    )
-    startDateTime: datetime.datetime = field(
-        metadata=dict()
-    )
-    startLat: float = field(
-        metadata=dict()
-    )
-    startLon: float = field(
-        metadata=dict()
-    )
-    endDateTime: datetime.datetime = field(
-        metadata=dict()
-    )
-    endLat: float = field(
-        metadata=dict()
-    )
-    endLon: float = field(
-        metadata=dict()
-    )
-    drogueOffDateTime: str = field(
-        metadata=dict()
-    )
-    deathCode: int = field(
-        metadata=dict()
-    )
+def _parse_datetime_with_day_ratio(monthSeries: np.ndarray, day_series: np.ndarray, year_series: np.ndarray):
+    values = list()
+    for month, day_with_ratio, year in zip(monthSeries, day_series, year_series):
+        day = day_with_ratio // 1
+        dayratio = day_with_ratio - day
+        seconds = dayratio * _SECONDS_IN_DAY
+        dt_ns = (datetime.datetime(year=int(year), month=int(month), day=int(day)) + datetime.timedelta(seconds=seconds)).timestamp() * 10**9
+        values.append(int(dt_ns))
+    return np.array(values, dtype="datetime64[ns]")
 
 
 @dataclass
-class RawDrifterBase:
-    id: int = field(
-        metadata=dict()
-    )
+class ParsingConfiguration:
+    cols: list[str]
+    coords: list[str]
+    col_dtypes: dict[str, np.dtype]
+    transform: dict[str, tuple[list[str], Callable[..., np.ndarray]]]
 
-    @classmethod
-    @abstractmethod
-    def fromcols(cls, cols: list[str]) -> RawDrifterBase:
-        raise NotImplementedError()
+    def get_vars_config_map(self, dim_name: str, df: pd.DataFrame):
+        all_config_map = self._get_all_config_map(dim_name, df)
+        return {col: all_config_map[col] for col in all_config_map.keys() if col not in self.coords}
 
+    def get_coords_config_map(self, dim_name: str, df: pd.DataFrame):
+        all_config_map = self._get_all_config_map(dim_name, df)
+        return {col: all_config_map[col] for col in self.coords}
 
-@dataclass
-class RawDrifterPosition:
-    positionDateTime: datetime.datetime = field(
-        metadata=dict()
-    )
-    lat: float = field(
-        metadata=dict()
-    )
-    lon: float = field(
-        metadata=dict()
-    )
-    qualityIndex: float
+    def _get_all_config_map(self, dim_name: str, df: pd.DataFrame):
+        pre_transform = {col: ([dim_name], df[[col]].to_numpy().flatten()) for col in self.cols}
+        post_transform = self._apply_transform(pre_transform)
+        return post_transform
 
-
-@dataclass
-class RawDrifterSensor:
-    sensorDateTime: datetime.datetime = field(
-        metadata=dict()
-    )
-    drogue: float = field(
-        metadata=dict()
-    )
-    sst: float = field(
-        metadata=dict()
-    )
-    voltage: float = field(
-        metadata=dict()
-    )
-    sensor4: float = field(
-        metadata=dict()
-    )
-    sensor5: float = field(
-        metadata=dict()
-    )
-    sensor6: float = field(
-        metadata=dict()
-    )
+    def _apply_transform(self, variable_config_map: dict[str, tuple[list[str], pd.DataFrame]]):
+        for output_col in self.transform.keys():
+            dim, input_cols, func = self.transform[output_col] 
+            args = list()
+            for col in input_cols:
+                _, variable = variable_config_map[col]
+                args.append(variable)
+            variable_config_map[output_col] = ([dim], func(*args))
+            [variable_config_map.pop(col) for col in input_cols]
+        return variable_config_map
 
 
-@dataclass
-class RawDrifterPRecord(RawDrifterPosition, RawDrifterBase):
-    @override
-    @classmethod
-    def fromcols(cls, cols: list[str]):
-        day = math.floor(float(cols[2]))
-        dayratio = float(cols[2]) - day
-        return cls(
-            id=int(cols[0]),
-            positionDateTime=datetime.datetime(
-                month=int(cols[1]),
-                day=day,
-                year=int(cols[3])
-            ) + datetime.timedelta(seconds=math.ceil(dayratio * _SECONDS_IN_DAY)),
-            lat=float(cols[4]),
-            lon=float(cols[5]),
-            qualityIndex=float(cols[6]),
+def _get_parsing_config(kind: _RecordKind) -> ParsingConfiguration:
+    return {
+        "position": ParsingConfiguration(
+            cols=["id", "obsMonth", "obsDay", "obsYear", "lat", "lon", "qualityIndex"],
+            col_dtypes={
+                "id": np.int64,
+                "obsMonth": np.int8,
+                "obsDay": np.float16,
+                "obsYear": np.int16,
+                "lat": np.float32,
+                "lon": np.float32,
+                "qualityIndex": np.float32
+            },
+            transform={
+                "obsDatetime": ("obs", ["obsMonth", "obsDay", "obsYear"], _parse_datetime_with_day_ratio)
+            },
+            coords=["id", "obsDatetime"]
+        ),
+        "sensor": ParsingConfiguration(
+            cols=["id", "obsMonth", "obsDay", "obsYear", "drogue", "sst", "voltage", "sensor4", "sensor5", "sensor6"],
+            col_dtypes={
+                "id": np.int64, 
+                "obsMonth": np.int8,
+                "obsDay": np.float16,
+                "obsYear": np.int16,
+                "drogue": np.int16,
+                "sst": np.float32,
+                "volate": np.float32,
+                "sensor4": np.float32,
+                "sensor5": np.float32,
+                "sensor6": np.float32
+            },
+            transform={
+                "obsDatetime": ("obs", ["obsMonth", "obsDay", "obsYear"], _parse_datetime_with_day_ratio)
+            },
+            coords=["id", "obsDatetime"]
+        ),
+        "both": ParsingConfiguration(
+            cols=["id", "posObsMonth", "posObsDay", "posObsYear", "lat", "lon", "senObsMonth", "senObsDay", "senObsYear", "drogue", "sst", "voltage", "sensor4", "sensor5", "sensor6", "qualityIndex"],
+            col_dtypes={
+                "id": np.int64, 
+                "posObsMonth": np.int8,
+                "posObsDay": np.float16,
+                "posObsYear": np.int16,
+                "lat": np.float32,
+                "lon": np.float32,
+                "qualityIndex": np.float32,
+                "senObsMonth": np.int8,
+                "senObsDay": np.float16,
+                "senObsYear": np.int16,
+                "drogue": np.int16,
+                "sst": np.float32,
+                "volate": np.float32,
+                "sensor4": np.float32,
+                "sensor5": np.float32,
+                "sensor6": np.float32,
+            },
+            transform={
+                "posObsDatetime": ("obs", ["posObsMonth", "posObsDay", "posObsYear"], _parse_datetime_with_day_ratio),
+                "sensorObsDatetime": ("obs", ["senObsMonth", "senObsDay", "senObsYear"], _parse_datetime_with_day_ratio)
+            },
+            coords=["id", "posObsDatetime", "sensorObsDatetime"]
         )
-
-
-@dataclass
-class RawDrifterSRecord(RawDrifterSensor, RawDrifterBase):
-    @override
-    @classmethod
-    def fromcols(cls, cols: list[str]):
-        day = math.floor(float(cols[2]))
-        dayratio = float(cols[2]) - day
-        return cls(
-            id=int(cols[0]),
-            sensorDateTime=datetime.datetime(
-                month=int(cols[1]),
-                day=day,
-                year=int(cols[3])
-            ) + datetime.timedelta(seconds=math.ceil(dayratio * _SECONDS_IN_DAY)),
-            drogue=float(cols[4]),
-            sst=float(cols[5]),
-            voltage=float(cols[6]),
-            sensor4=float(cols[7]),
-            sensor5=float(cols[8]),
-            sensor6=float(cols[9]),
-        )
-
-
-@dataclass
-class RawDrifterPSRecord(RawDrifterPosition, RawDrifterSensor, RawDrifterBase):
-    @override
-    @classmethod
-    def fromcols(cls, cols: list[str]):
-        pos_day = math.floor(float(cols[2]))
-        pos_dayratio = float(cols[2]) - pos_day
-
-        sensor_day = math.floor(float(cols[7]))
-        sensor_dayratio = float(cols[7]) - sensor_day
-        return cls(
-            id=int(cols[0]),
-            positionDateTime=datetime.datetime(
-                month=int(cols[1]),
-                day=pos_day,
-                year=int(cols[3])
-            ) + datetime.timedelta(seconds=math.ceil(pos_dayratio * _SECONDS_IN_DAY)),
-            lat=float(cols[4]),
-            lon=float(cols[5]),
-            sensorDateTime=datetime.datetime(
-                month=int(cols[6]),
-                day=sensor_day,
-                year=int(cols[8])
-            ) + datetime.timedelta(seconds=math.ceil(sensor_dayratio * _SECONDS_IN_DAY)),
-            drogue=float(cols[9]),
-            sst=float(cols[10]),
-            voltage=float(cols[11]),
-            sensor4=float(cols[12]),
-            sensor5=float(cols[13]),
-            sensor6=float(cols[14]),
-            qualityIndex=float(cols[15]),
-        )
-
-
-def _form_url(url: str, start: str, end: str, suffix: str) -> str:
-    filename = _FILNAME_TEMPLATE.format(start=start, end=end, suffix=suffix)
-    return f"{url}/{filename}"
-
-
-def _form_path(path: str, start: str, end: str, suffix: str) -> str:
-    filename = _FILNAME_TEMPLATE.format(start=start, end=end, suffix=suffix)
-    return os.path.join(path, filename)
-
-def _form_request(start, end, suffix):
-    return (_form_url(_DATA_URL, start, end, suffix), _form_path(_TMP_PATH, start, end, suffix))
-
-
-def _form_record(kind: _RecordKind, cols: list[str]):
-    constructor = {
-        "position": RawDrifterPRecord.fromcols,
-        "sensor": RawDrifterSRecord.fromcols,
-        "both": RawDrifterPSRecord.fromcols
     }.get(kind)
-    return constructor(cols)
 
 
 def _get_download_list(kind: _RecordKind) -> list[tuple[str, str]]:
@@ -233,76 +151,103 @@ def _get_download_list(kind: _RecordKind) -> list[tuple[str, str]]:
         (15_001, "current")
     ]
 
-    return [ 
-        _form_request(start, end, suffix) for start, end in batches
-    ]
+    requests = list()
 
+    for start, end in batches:
+        filename = _FILNAME_TEMPLATE.format(start=start, end=end, suffix=suffix)
+        requests.append((f"{_DATA_URL}/{filename}", os.path.join(_TMP_PATH, filename)))
+    return requests
 
-def _load_gdp_metadata() -> dict:
-    gdp_metadata_df = get_gdp_metadata()
-    gdp_metadata = dict()
+def setup_preprocess(md_df: pd.DataFrame, data_df: pd.DataFrame, config: ParsingConfiguration):
+    def wrapped_preprocess(id_) -> xr.Dataset:
+        traj_md_df = md_df[md_df["ID"] == id_[0]]
+        traj_data_df = data_df[data_df["id"] == id_[0]]
 
-    for rowidx in tqdm(range(len(gdp_metadata_df)), desc="loading drifter metadata into memory"):
-        drifter_md_row = gdp_metadata_df.iloc[rowidx]
-        drifter_id = drifter_md_row[["ID"]].values[0]
-        drifter_md = DrifterMetadata(
-            id=int(drifter_id),
-            wmoNumber=int(drifter_md_row[["WMO_number"]].values[0]),
-            programNumber=int(drifter_md_row[["program_number"]].values[0]),
-            buoyType=drifter_md_row[["buoys_type"]].values[0],
-            drogueOffDateTime=drifter_md_row[["Drogue_off_date"]].values[0],
-            startDateTime=drifter_md_row[["Start_date"]].values[0],
-            startLat=float(drifter_md_row[["Start_lat"]].values[0]),
-            startLon=float(drifter_md_row[["Start_lon"]].values[0]),
-            endDateTime=drifter_md_row[["End_date"]].values[0],
-            endLat=float(drifter_md_row[["End_lat"]].values[0]),
-            endLon=float(drifter_md_row[["End_lon"]].values[0]),
-            deathCode=drifter_md_row[["death_code"]].values[0],
+        variables = {
+            "wmo_number": (["traj"], traj_md_df[["WMO_number"]].values[0]),
+            "program_number": (["traj"], traj_md_df[["program_number"]].values[0]),
+            "buoys_type": (["traj"], traj_md_df[["buoys_type"]].values[0]),
+            "start_date": (["traj"], traj_md_df[["Start_date"]].values[0]),
+            "start_lat": (["traj"], traj_md_df[["Start_lat"]].values[0]),
+            "start_lon": (["traj"], traj_md_df[["Start_lon"]].values[0]),
+            "end_date": (["traj"], traj_md_df[["End_date"]].values[0]),
+            "end_lat": (["traj"], traj_md_df[["End_lat"]].values[0]),
+            "end_lon": (["traj"], traj_md_df[["End_lon"]].values[0]),
+            "drogue_off_date": (["traj"], traj_md_df[["Drogue_off_date"]].values[0]),
+            "death_code": (["traj"], traj_md_df[["death_code"]].values[0]),
+        }
+
+        data_variables = config.get_vars_config_map("obs", traj_data_df)
+        coords = config.get_coords_config_map("obs", traj_data_df)
+        row_coord = {
+            "id": (["traj"], traj_md_df[["ID"]].values[0]),
+        }
+
+        variables.update(data_variables),
+        coords.update(row_coord),
+        
+        dataset = xr.Dataset(
+            variables,
+            coords=coords
         )
-        gdp_metadata[drifter_id] = drifter_md
-    return gdp_metadata
-
-def _download(kind: _RecordKind):
-
-    gdp_metadata_table = _load_gdp_metadata()
-    requests = _get_download_list(kind)
-    download_with_progress(requests)
-    destinations = [dst for (_, dst) in requests]
-    data_files = list()
-    for fp in tqdm(destinations, desc="decompressing raw data files"):
-        uncompressed_fp = fp[:-3]
-        if os.path.exists(uncompressed_fp):
-            data_files.append(uncompressed_fp)
-            continue
-
-        with gzip.open(fp, "r") as cf, open(uncompressed_fp, "w") as f:
-            while((data := cf.read()) != b""):
-                f.write(data.decode("utf-8"))
-            data_files.append(uncompressed_fp)
-    
-    mapped_records = defaultdict[str, list](list)
-
-    trashed_rows = 0
-    for fp in data_files:
-        with open(fp, "r") as f, tqdm(desc=f"Loading records from ({fp}) into memory") as bar:
-            while ((row := f.readline()) != ""):
-                cols = row.split()
-                id_ = int(cols[0])
-
-                try:
-                    record = _form_record(kind, cols)
-                    mapped_records[id_].append(record)
-                    bar.update()
-                except Exception as e:
-                    _logger.debug(f"Data file ({fp}) contains invalid row ({row}). Error: ({e})")
-                    trashed_rows += 1
-    _logger.info(f"trashed {trashed_rows} rows due to an exception during load time")
-    print("hodor")
+        return dataset
+    return wrapped_preprocess
 
 
 def to_raggedarray(
     kind: _RecordKind = "both",
     tmp_path: str = _TMP_PATH,
 ) -> RaggedArray:
-    _download(kind)
-    ...
+    gdp_metadata_df = get_gdp_metadata()
+
+    config = _get_parsing_config(kind)
+    requests = _get_download_list(kind)
+    download_with_progress(requests[:1])
+    destinations = [dst for (_, dst) in requests]
+
+    df: pd.DataFrame = None
+    for fp in tqdm(destinations[:1], desc="loading data files"):
+        before = datetime.datetime.now()
+
+        cur_df = pd.read_csv(
+            fp, 
+            sep=r"\s+",
+            header=None,
+            names=config.cols,
+            engine="c",
+            compression="gzip",
+        )
+
+        after = datetime.datetime.now()
+
+        _logger.debug(f"elapsed time to load {len(cur_df)} records :: {after - before}")
+
+        if df is None:
+            df = cur_df
+        else:
+            df = pd.concat([df, cur_df])
+
+    
+    ra = RaggedArray.from_files(
+        indices=gdp_metadata_df[["ID"]].values,
+        preprocess_func=setup_preprocess(gdp_metadata_df, df, config),
+        name_coords=config.coords,
+        name_meta=[
+            "ID",
+            "WMO_number",
+            "program_number",
+            "buoys_type",
+            "Start_date",
+            "Start_lat",
+            "Start_lon",
+            "End_date",
+            "End_lat",
+            "End_lon",
+            "Drogue_off_date",
+            "death_code",
+        ],
+        name_data=["lat", "lon", "drogue", "sst", "voltage", "sensor4", "sensor5", "sensor6"],
+        name_dims={"traj": "rows", "obs": "obs"},
+        rowsize_func=lambda id_: len(df[df["id"] == id_[0]])
+    )
+    return ra
