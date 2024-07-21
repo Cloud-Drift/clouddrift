23c6b77532ee96f824cecdd97b879d368301a89f
Kevin Santana <kevinsantana11@gmail.com>
2024-07-21 16:03:18 -0400

    fmt

--- clouddrift/adapters/gdp1h.py
+++ clouddrift/adapters/gdp1h.py
@@ -139,7 +139,7 @@ def preprocess(index: int, **kwargs) -> xr.Dataset:
         fp,
         decode_times=False,
         decode_coords=False,
-        engine = "netcdf4",
+        engine="netcdf4",
     )
 
     # parse the date with custom function
