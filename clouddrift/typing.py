28eabf8d6ec8fd6102c2fdf2cf711b1f491b77d7
Kevin Santana <kevinsantana11@gmail.com>
2024-08-20 00:39:30 -0400

    typevar not needed

--- clouddrift/typing.py
+++ clouddrift/typing.py
@@ -1,11 +1,10 @@
 from datetime import timedelta
-from typing import Any, TypeVar
+from typing import Any
 
 import numpy as np
 import pandas as pd
 import xarray as xr
 
-T = TypeVar("T")
 _SupportedArrayTypes = (
     list[Any] | np.ndarray[Any, np.dtype[Any]] | xr.DataArray | pd.Series[Any]
 )
