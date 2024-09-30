19ed39ce677f8db4cefaa127da374568d7091e79
Kevin Santana <kevinsantana11@gmail.com>
2024-09-30 16:20:09 -0700

    🔍 gdp6h adapter ragged array metadata update

--- clouddrift/adapters/gdp/gdp6h.py
+++ clouddrift/adapters/gdp/gdp6h.py
@@ -18,7 +18,7 @@ import clouddrift.adapters.gdp as gdp
 from clouddrift.adapters.utils import download_with_progress, standard_retry_protocol
 from clouddrift.raggedarray import RaggedArray
 
-GDP_VERSION = "September 2023"
+GDP_VERSION = "April 2024"
 
 GDP_DATA_URL = "https://www.aoml.noaa.gov/ftp/pub/phod/buoydata/6h"
 GDP_TMP_PATH = os.path.join(tempfile.gettempdir(), "clouddrift", "gdp6h")
