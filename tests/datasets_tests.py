516d688cbeb071fd67a09d827d6b70ce46c9bc85
Kevin Santana <kevinsantana11@gmail.com>
2025-02-18 13:40:03 -0500

    ⭐️ IBTRACS dataset adapter (#493)
    * ibtracs adapter

--- tests/datasets_tests.py
+++ tests/datasets_tests.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import tests.utils as testutils
-from clouddrift import datasets
+from clouddrift import adapters, datasets
 from clouddrift.ragged import apply_ragged, subset
 
 
@@ -13,6 +13,42 @@ class datasets_tests(testutils.DisableProgressTestCase):
     def test_gdp6h(self):
         with datasets.gdp6h() as ds:
             self.assertTrue(ds)
+
+    def test_ibtracs(self):
+        """
+        The original dataset contains data points for entire time span of the dataset. Due to this, we trim
+        the original data array to only contain up to the numobs associated to the storm. In this test we validate
+        the trimmed data and ensure the left over is all NaN.
+        """
+        options = dict(version="v04r01", kind="LAST_3_YEARS")
+        ragged_ds = adapters.ibtracs.to_raggedarray(**options)
+        ds = adapters.ibtracs._get_original_dataset(**options)
+        ragged_ds_first = subset(
+            ragged_ds, {"storm": 0}, row_dim_name="storm", rowsize_var_name="numobs"
+        )
+        ds_first = ds.sel(storm=0)
+        first_numobs: np.ndarray = ds_first.numobs.data
+        first_numobs = first_numobs.astype(int)
+
+        # Both transformed and original data array should contain the same values
+        self.assertTrue(
+            np.all(
+                np.allclose(
+                    ds_first.usa_r34.data[:first_numobs],
+                    ragged_ds_first.usa_r34.data,
+                    equal_nan=True,
+                )
+            )
+        )
+
+        # The rest of the values should be nan
+        self.assertTrue(
+            np.all(
+                np.isnan(
+                    ds_first.usa_r34.data[first_numobs + 1 :],
+                )
+            )
+        )
 
     def test_glad(self):
         with datasets.glad() as ds:
